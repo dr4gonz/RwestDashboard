ae8c60b0a496a7115ecc8ce53100c82acab3f8cb
Carl Egbert <egbertcarl@gmail.com>
2016-10-18 15:38:26 -0700

    cleanup remainders from template, change readme

--- ng2auth/src/app/app.component.ts
+++ ng2auth/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Component({
-  selector: 'daily-deals',
+  selector: 'app-component',
   templateUrl: 'app.component.html',
   styleUrls : ['app.component.css']
 })
 export class AppComponent {
 
-  title = 'Daily Deals';
+  title = 'R/West Angular2 Login Mockup';
 
   constructor(private authService: AuthService) {}
 }
