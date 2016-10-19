d8561bef9ec9070375b6102deb42df5a392e34f3
Matthew <mreyez@gmail.com>
2016-10-19 14:07:09 -0700

    continue working on angular 2 get request w/ headers

--- ng2auth/src/app/app.component.ts
+++ ng2auth/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
+import { FileService } from './file.service';
 
 @Component({
   selector: 'app-component',
   templateUrl: 'app.component.html',
-  styleUrls : ['app.component.css']
+  styleUrls : ['app.component.css'],
+  providers: [ FileService ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'R/West Angular2 Login Mockup';
+  response: any;
+  errorMessage: string;
+  constructor(private authService: AuthService, private fileService: FileService) { }
 
-  constructor(private authService: AuthService) {}
+  ngOnInit() { this.getUser(); }
+  getUser() {
+    this.fileService.userLogin().subscribe( res => this.response = res, error => this.errorMessage = <any>error);
+  }
 }
