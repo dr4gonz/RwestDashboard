fc823afcad8b9403a2cfa719d10aa1fe0194e486
Carl Egbert <egbertcarl@gmail.com>
2016-10-20 13:04:09 -0700

    config firebase

--- ng2auth/src/app/app.module.ts
+++ ng2auth/src/app/app.module.ts
@@ -3,23 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { AUTH_PROVIDERS } from 'angular2-jwt';
+import { AngularFireModule } from 'angularfire2';
+
+import { Keys } from '../keys';
 
 import { AppComponent } from './app.component';
 import { LinksComponent } from './links/links.component';
 import { LinksAdminComponent } from './links-admin/links-admin.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
 import { routing, routedComponents } from './app.routing';
 
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service'
 import { AuthGuardAdmin } from './auth-guard-admin.service';
+
+export const firebaseConfig = Keys.FireBaseConfig;
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     routing,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   declarations: [
     AppComponent,
--- ng2auth/src/keys.ts
+++ ng2auth/src/keys.ts
@@ -1,4 +1,11 @@
 export class Keys {
-  Auth0Domain: string = 'carlegbert.auth0.com';
-  Auth0ClientId: string = 'UPewrAsrOxeHQ4kxeUcBodCS2a7f0xwe'
+  static Auth0Domain: string = 'carlegbert.auth0.com';
+  static Auth0ClientId: string = 'UPewrAsrOxeHQ4kxeUcBodCS2a7f0xwe'
+  static FireBaseConfig = {
+    apiKey: "AIzaSyC2dQejpEaSFmBrgs02tW2Ol_1m78atI0M",
+    authDomain: "rwest-ng2-app.firebaseapp.com",
+    databaseURL: "https://rwest-ng2-app.firebaseio.com",
+    storageBucket: "rwest-ng2-app.appspot.com",
+    messagingSenderId: "876488378038"
+  }
 }
