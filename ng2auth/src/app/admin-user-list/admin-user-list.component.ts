5978ca010c135717cdd33fb6d5386c2c2bf54309
Carl Egbert <egbertcarl@gmail.com>
2016-11-09 15:41:19 -0800

    refactor admin component so that list updates when new user is created, minor layout improvement

--- ng2auth/src/app/admin-user-list/admin-user-list.component.ts
+++ ng2auth/src/app/admin-user-list/admin-user-list.component.ts
@@ -4,6 +4,7 @@ import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-admin-user-list',
+  inputs: ['users'],
   templateUrl: './admin-user-list.component.html',
   styleUrls: ['./admin-user-list.component.css']
 })
@@ -12,8 +13,7 @@ export class AdminUserListComponent implements OnInit {
 
   users: FirebaseListObservable<User[]>;
 
-  constructor(private af: AngularFire, @Inject(FirebaseApp) firebase: any) {
-    this.users = af.database.list('/users');
+  constructor() {
   }
 
   ngOnInit() {
